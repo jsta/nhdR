f7f7016e5ef72c31b8bc1601c085d885719be35c
jsta <jstachelek@utexas.edu>
2017-03-01 21:03:59 -0500

    query takes multiple dsns

--- R/query.R
+++ R/query.R
@@ -1,18 +1,23 @@
 #' nhd_plus_query
 #' @export
+#' @param lon numeric longitude
+#' @param lat numeric latitude
+#' @param dsn character data source
+#' @param buffer_dist numeric buffer in units of coordinate degrees
 #' @examples \dontrun{
 #' # Lake Lashaway
 #' wk <- wikilake::lake_wiki("Gull Lake (Michigan)")
-#' qry <- nhd_plus_query(wk$Lon, wk$Lat, dsn = "NHDWaterbody")
+#' qry <- nhd_plus_query(wk$Lon, wk$Lat, dsn = c("NHDWaterbody", "NHDFlowLine"))
 #'
-#' plot(qry$sp$geometry, col = "blue")
+#' plot(qry$sp$NHDWaterbody$geometry, col = "blue")
+#' plot(qry$sp$NHDFlowLine$geometry, col = "cyan", add = TRUE)
 #' plot(qry$pnt, col = "red", pch = 19, add = TRUE)
 #' axis(1); axis(2)
 #' }
 
-nhd_plus_query <- function(lon, lat, dsn){
+nhd_plus_query <- function(lon, lat, dsn, buffer_dist = 0.05){
   pnt <- sf::st_sfc(sf::st_point(c(lon, lat)))
-  pnt_buff  <- sf::st_sfc(sf::st_buffer(pnt, dist = 0.05))
+  pnt_buff  <- sf::st_sfc(sf::st_buffer(pnt, dist = buffer_dist))
   sf::st_crs(pnt_buff) <- sf::st_crs(pnt) <- sf::st_crs(nhdR::vpu_shp)
 
   vpu <- sf::st_covers(nhdR::vpu_shp$geometry, pnt)
@@ -20,19 +25,23 @@ nhd_plus_query <- function(lon, lat, dsn){
   vpu <- suppressWarnings(as.numeric(as.character(vpu$UnitID)))
   vpu <- vpu[!is.na(vpu)]
 
-  sp <- nhd_plus_load(vpu = vpu, dsn = dsn)
-  sf::st_crs(sp) <- 4269
+  sp <- lapply(dsn, function(x) nhd_plus_load(vpu = vpu, dsn = x))
+  for(i in 1:length(sp)){
+    sf::st_crs(sp[[i]]) <- 4269
+  }
 
-  sp       <- sf::st_transform(sp, crs = "+proj=utm +zone=10 +datum=WGS84")
+  sp       <- lapply(sp, function(x) sf::st_transform(x,
+                crs = "+proj=utm +zone=10 +datum=WGS84"))
   pnt      <- sf::st_transform(pnt, crs = "+proj=utm +zone=10 +datum=WGS84")
   pnt_buff <- sf::st_transform(pnt_buff,
                 crs = "+proj=utm +zone=10 +datum=WGS84")
 
+  sp_intersecting <- lapply(sp, function(x) unlist(lapply(sf::st_intersects(x, pnt_buff), length)) > 0)
 
-  sp_intersecting <- unlist(
-                      lapply(sf::st_intersects(sp, pnt_buff), length)) > 0
   # check if any(sp_intersecting)
-  sp_sub <- sp[sp_intersecting,]
+  sp_sub <- lapply(1:length(sp_intersecting),
+                   function(x) sp[[x]][sp_intersecting[[x]],])
+  names(sp_sub) <- dsn
 
   list(pnt = pnt, sp = sp_sub)
 }
