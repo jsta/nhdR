e057c9ac9f5c3cb300c9ab69e9080e7a66bc2cdc
jsta <jstachelek@utexas.edu>
2017-06-29 11:38:16 -0400

    allow vpus to be specified with pre-padded zeros, closes #15

--- R/get.R
+++ R/get.R
@@ -44,7 +44,8 @@ nhd_get <- function(state = NA){
 #' }
 nhd_plus_get <- function(vpu = NA, component = "NHDSnapshot"){
 
-  if(!vpu %in% c("National", 1:22, "10L", "10U", "03N", "03W", "03S")){
+  if(!vpu %in% c("National", 1:22, "10L", "10U", "03N", "03W", "03S",
+                 paste0("0", 1:9))){
     stop(paste0(vpu, " is not a valid vpu"))
   }
 
