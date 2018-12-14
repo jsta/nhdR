faa13267054101adf06efedab75c2cd243ff3d7e
jsta <stachel2@msu.edu>
2018-12-14 16:57:36 -0500

    dont print warning on force_unzip

--- R/get.R
+++ R/get.R
@@ -88,10 +88,11 @@ nhd_plus_get <- function(vpu = NA, component = "NHDSnapshot", force_dl = FALSE,
   if(force_unzip & !force_dl){
     system(paste0(has_7z()$path, " e ", shQuote(destfile),
                   " -o", shQuote(destsubdir)))
-  }
-  if(get_if_not_exists(url, destfile, force_dl = force_dl)){
-    system(paste0(has_7z()$path, " e ", shQuote(destfile),
-                  " -o", shQuote(destsubdir)))
+  }else{
+    if(get_if_not_exists(url, destfile, force_dl = force_dl)){
+      system(paste0(has_7z()$path, " e ", shQuote(destfile),
+                    " -o", shQuote(destsubdir)))
+    }
   }
 }
 
