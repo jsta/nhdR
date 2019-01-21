8819e685f33a97d1fd52c0c03378db323dca5fdd
jsta <stachel2@msu.edu>
2019-01-21 12:35:13 -0500

    pass startup checks

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-.onLoad <- function(lib, pkg){
+.onAttach <- function(lib, pkg){
   if(!has_7z()$yes){
     packageStartupMessage("The 7-zip program is needed to unpack NHD downloads (http://www.7-zip.org/).")
   }
