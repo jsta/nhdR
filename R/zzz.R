190ccfb644494c47afb278788a666ef74a31ca79
jsta <stachel2@msu.edu>
2019-02-12 14:38:02 -0500

    exclude from coverage stats

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,7 @@
+# nocov start
 .onAttach <- function(lib, pkg){
   if(!has_7z()$yes){
     packageStartupMessage("The 7-zip program is needed to unpack NHD downloads (http://www.7-zip.org/).")
   }
 }
+# nocov end
