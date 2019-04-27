fa7e18e3c4624bd960ef49fdd4e913ad493dbc14
jsta <stachel2@msu.edu>
2019-04-27 17:05:42 -0400

    zoom in for logo

--- logo/logo_hex-streams.R
+++ logo/logo_hex-streams.R
@@ -12,7 +12,7 @@ library(sp)
 # ---- function to intersect text glyph with watershed ----
 # https://djnavarro.net/post/in-between.html
 
-glyph_sf <- function(chars, bbox, scale_factor = 3700){
+glyph_sf <- function(chars, bbox, scale_factor = 3700, offset_y = 0){
   # chars <- "nhdR"
   # bbox <- hu12
   n_raw_mat <- lapply(strsplit(chars, "")[[1]], function(x){
@@ -21,7 +21,7 @@ glyph_sf <- function(chars, bbox, scale_factor = 3700){
     centroid    <- as.data.frame(st_coordinates(centroid))
     # multiplication avoids rounding errors
     n_raw$x     <- (n_raw$x * 10) + centroid$X
-    n_raw$y     <- (n_raw$y * 10) + centroid$Y
+    n_raw$y     <- (n_raw$y * 10) + centroid$Y + offset_y
 
     if(any(is.na(n_raw$x))){
       list(
@@ -82,17 +82,18 @@ lakes        <- st_transform(lake_streams$sp$NHDWaterbody, st_crs(hu12))
 # ---- clip streams to font glyph and scale ----
 # http://www.katiejolly.io/blog/2019-01-21/map-cutouts
 
-scale_factor_hex     <- 4.2
-scale_factor_n       <- 3700
+scale_factor_hex     <- 1
+scale_factor_n       <- scale_factor_hex * 880.95
 
 ncg                  <- st_geometry(hex_template)
 cntrd                <- st_centroid(ncg)
 hex_template_scaled  <- (ncg - cntrd) * scale_factor_hex + cntrd
 hex_template_scaled  <- st_sfc(hex_template_scaled, crs = st_crs(hex_template))
 
-n         <- glyph_sf("nhdR", hu12, scale_factor = scale_factor_n)
-n_bbox    <- st_intersection(streams, st_as_sfc(st_bbox(n)))
-n_streams <- st_intersection(n_bbox, n)
+n         <- glyph_sf("nhdR", hu12, scale_factor = scale_factor_n, offset_y = -13500)
+
+# n_bbox    <- st_intersection(streams, st_as_sfc(st_bbox(n)))
+# n_streams <- st_intersection(n_bbox, n)
 
 # ---- clip streams to hexagon ----
 
@@ -107,6 +108,7 @@ gg_logo <- ggplot() +
     geom_sf(data = n, size = 0.6, fill = "white") +
     coord_sf(datum = NA) +
     cowplot::theme_nothing()
+gg_logo
 
 hexSticker::sticker(gg_logo, s_x = 1, s_y = 1, s_width = 2.17, s_height = 2.17,
                     package = "", filename = "logo/logo.png",
