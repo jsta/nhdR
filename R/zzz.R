83d5b8de0a5aad97fd7c49badf762b45e1ce4a79
jsta <stachel2@msu.edu>
2021-10-26 16:46:56 -0600

    linting

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,12 @@
 # nocov start
-.onAttach <- function(lib, pkg){
-  if(!has_7z()$yes){
+.onAttach <- function(lib, pkg) {
+  if (!has_7z()$yes) {
     packageStartupMessage("The 7-zip program is needed to unpack NHD downloads (http://www.7-zip.org/).")
   }
 }
 
 .onLoad <- function(libname, pkgname) {
   nhd_plus_load <<- memoise::memoise(nhd_plus_load,
-                                     cache = memoise::cache_memory())
+    cache = memoise::cache_memory())
 }
 # nocov end
